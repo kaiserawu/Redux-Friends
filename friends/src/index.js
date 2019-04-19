d54c5b92d87f114772d642328f252fe51558229c
kaiserawu <kaiser.a.wu@gmail.com>
2019-04-18 19:09:03 -0700

    Initialize redux structure

--- friends/src/index.js
+++ friends/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './App';
+import rootReducer from './reducers';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,14 @@
+import { /*actions to be added here*/ } from '../actions';
+
+const initialState = {
+  friends: []
+}
+
+const rootReducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default rootReducer;
